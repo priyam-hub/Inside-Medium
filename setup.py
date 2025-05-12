d1483a0e243338a1d0aecd08a221ea5f76c55a07
priyam-hub <palpriyam95@gmail.com>
2025-05-12 18:44:00 +0530

    initial upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+# DEPENDENCIES
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name             = 'Inside_Medium',
+    version          = '0.0.0',
+    author           = 'Priyam Pal',
+    author_email     = 'palpriyam95@gmail.com',
+    packages         = find_packages(),
+    install_requires = []
+
+)
